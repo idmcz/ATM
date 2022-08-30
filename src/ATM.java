8caf27b85266f7bb2cf800600c6bd8dc94b88df2
idalis <dalibucks@gmail.com>
2022-08-30 12:15:40 -0700

    depositMoney, checkBalance

--- src/ATM.java
+++ src/ATM.java
@@ -1,4 +1,38 @@
+import java.util.HashMap;
 
 public class ATM {
-
+	HashMap<Integer, Double> hashie = new HashMap<Integer, Double>();
+	
+	public ATM () {
+		
+	}
+	
+	public void openAccount (int accountNum) {
+		hashie.put(accountNum, 0.0);
+	}
+	
+	public void openAccount (int accountNum, double deposit) {
+		hashie.put(accountNum, deposit);
+	}
+	
+	public void closeAccount (int accountNum) {
+		if (hashie.get(accountNum)==0) {
+			hashie.remove(accountNum);
+		}
+	}
+	
+	public double checkBalance (int accountNum) { 
+		if (hashie.containsKey(accountNum))
+		return hashie.get (accountNum);
+		return 0.0;
+	}
+	
+	public boolean depositMoney (int accountNum, double deposit) {
+		if (hashie.containsKey(accountNum)) {
+			double cur = hashie.get(accountNum); 
+			hashie.put (accountNum, cur + deposit);
+			return true;
+		}
+		return false;
+	}
 }
